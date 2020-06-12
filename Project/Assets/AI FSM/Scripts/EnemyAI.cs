1ff6453f8691199ad02792349776a5c095b75788
Christian <chrisarg.otico@gmail.com>
2020-06-12 10:55:56 +0800

    Ai

--- Project/Assets/AI FSM/Scripts/EnemyAI.cs
+++ Project/Assets/AI FSM/Scripts/EnemyAI.cs
@@ -68,6 +68,8 @@ public class EnemyAI : MonoBehaviour
             anim.SetTrigger("isDead");
             Instantiate(bloodSplatter, transform.position, transform.rotation);
         }
+
+
         //HP <= 0 sethp == 0
 
     }
