5c14299daaae96489187d071d4c04f3db5f76dc7
mbmartin929 <martinlouie56@gmail.com>
2020-06-20 10:33:14 +0800

    URP Conversion, and Blood shaders WIP

--- Project/Assets/Scripts/LooksAtPlayer.cs
+++ Project/Assets/Scripts/LooksAtPlayer.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LooksAtPlayer : MonoBehaviour
 {
+    public enum LookMode { LookAway, LookTowards };
+    public LookMode _LookMode;
+
     private GameObject target;
 
     // Start is called before the first frame update
@@ -24,6 +27,13 @@ public class LooksAtPlayer : MonoBehaviour
                                      transform.position.y,
                                      target.transform.position.z);
 
-        transform.LookAt(targetRotation);
+        if (_LookMode == LookMode.LookTowards) transform.LookAt(targetRotation);
+        else if (_LookMode == LookMode.LookAway) LookAwayFrom(targetRotation);
+    }
+
+    private void LookAwayFrom(Vector3 point)
+    {
+        point = 2.0f * transform.position - point;
+        transform.LookAt(point);
     }
 }
