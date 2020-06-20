1e9ba1ab10ef67d97763fab4af97808c95314be6
Christian <chrisarg.otico@gmail.com>
2020-06-20 18:37:01 +0800

    Test
    Test

--- Project/Assets/Scripts/Items/HealthPotion.cs
+++ Project/Assets/Scripts/Items/HealthPotion.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthPotion : PickUpController
 {
 
     private int HealAmountHp;
 
-     void Start()
+    void Start()
     {
         PickUpController Potion = new PickUpController();
         Potion.itemName = "Heart";
@@ -18,14 +19,20 @@ public class HealthPotion : PickUpController
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
-        {    
-            
+        {
+            overlayImage.enabled = true;
             unit.CurHealth += healAmount;
             Debug.Log("HP PICKED!" + healAmount);
             Destroy(this.gameObject);
-
         }
+
     }
 
 
+
+
 }
+
+
+
+
