685f779b3ec0058cd29b8f4a262afe92c14cc1d3
mbmartin929 <martinlouie56@gmail.com>
2020-06-22 13:46:22 +0800

    8D Sprite System WIP
    SO FKIN HUGE MILESTONE AAAAAAAAAAAAAAAA

---
+++ Project/Assets/Scripts/BlendTreeTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendTreeTest : MonoBehaviour
+{
+    private Animator animator;
+
+    private float x;
+    private float z;
+
+    private GameObject playerGo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerGo = GameManager.Instance.playerGo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // x = transform.position.x + (playerGo.transform.position.x - transform.position.x) / 2;
+        // z = transform.position.z + (playerGo.transform.position.z - transform.position.z) / 2;
+
+        x = transform.position.x - playerGo.transform.position.x;
+        z = transform.position.z - playerGo.transform.position.z;
+
+        animator.SetFloat("X", x);
+        animator.SetFloat("Z", z);
+    }
+}
---
+++ Project/Assets/Scripts/EnemyDirectionalSprite.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDirectionalSprite : MonoBehaviour
+{
+    public GameObject[] faces;
+
+    private int directionID = 0;
+
+    private TempNavMesh tempNavMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempNavMesh = GetComponent<TempNavMesh>();
+        //Debug.Log(tempNavMesh.gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        directionID = tempNavMesh.waypointID;
+
+        for (int i = 0; i < faces.Length; i++)
+        {
+            if (i == directionID)
+            {
+                faces[i].SetActive(true);
+                //planes[i].GetComponent<Animator>().SetInteger("directionID", directionID);
+            }
+            else faces[i].SetActive(false);
+        }
+    }
+}
