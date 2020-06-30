814f3cccc4c14e0830659f5aba6953e0fbfb2d86
mbmartin929 <martinlouie56@gmail.com>
2020-06-30 11:34:46 +0800

    AI Detection update

--- Project/Assets/AI FSM/Scripts/EnemyAI.cs
+++ Project/Assets/AI FSM/Scripts/EnemyAI.cs
@@ -22,6 +22,16 @@ namespace EightDirectionalSpriteSystem
         private GameObject playerGo;
         public EnemyController enemyController;
 
+        [Header("AI Detection")]
+        private Camera viewCamera;
+
+        public float viewRadius;
+        [Range(0, 360)]
+        public float viewAngle;
+        public LayerMask targetMask;
+        public LayerMask obstacleMask;
+        public List<Transform> visibleTargets = new List<Transform>();
+
         void Awake()
         {
             //enemyController = GetComponent<EnemyController>();
@@ -34,6 +44,10 @@ namespace EightDirectionalSpriteSystem
             anim = GetComponent<Animator>();
 
             actor = GetComponent<DemoActor>();
+
+            viewCamera = Camera.main;
+
+            StartCoroutine("FindTargetsWithDelay", .2f);
         }
 
         // Update is called once per frame
@@ -53,42 +67,86 @@ namespace EightDirectionalSpriteSystem
         {
             //Debug.Log("Enemy Raycast");
 
-            #region  Raycast info
-            RaycastHit hitInfoForward;
-            RaycastHit hitInfoLeft;
-            RaycastHit hitInfoRight;
+            // #region  Raycast info
+            // RaycastHit hitInfoForward;
+            // RaycastHit hitInfoLeft;
+            // RaycastHit hitInfoRight;
 
-            Ray forwardRay = new Ray(transform.position, transform.forward * rayCastdistance);
-            Ray rightRay = new Ray(transform.position, (transform.forward * 2 - transform.right) * rayCastdistance);
-            Ray leftRay = new Ray(transform.position, (transform.forward * 2 - (-transform.right)) * rayCastdistance);
-            /// <summary>
-            // Draws Green Line for raycast visual aide
-            // new vector 3 serves as offset for raycast
-            /// </summary>
+            // Ray forwardRay = new Ray(transform.position, transform.forward * rayCastdistance);
+            // Ray rightRay = new Ray(transform.position, (transform.forward * 2 - transform.right) * rayCastdistance);
+            // Ray leftRay = new Ray(transform.position, (transform.forward * 2 - (-transform.right)) * rayCastdistance);
+            // /// <summary>
+            // // Draws Green Line for raycast visual aide
+            // // new vector 3 serves as offset for raycast
+            // /// </summary>
 
-            Debug.DrawRay(transform.position, transform.forward * rayCastdistance, Color.green);
-            Debug.DrawRay(transform.position, (transform.forward * 2 - transform.right) * rayCastdistance, Color.green);
-            Debug.DrawRay(transform.position, (transform.forward * 2 - (-transform.right)) * rayCastdistance, Color.green);
-            #endregion
+            // Debug.DrawRay(transform.position, transform.forward * rayCastdistance, Color.green);
+            // Debug.DrawRay(transform.position, (transform.forward * 2 - transform.right) * rayCastdistance, Color.green);
+            // Debug.DrawRay(transform.position, (transform.forward * 2 - (-transform.right)) * rayCastdistance, Color.green);
+            // #endregion
 
-            #region Detects Player
-            if (Physics.Raycast(forwardRay, out hitInfoForward, rayCastdistance))
+            // #region Detects Player
+            // if (Physics.Raycast(forwardRay, out hitInfoForward, rayCastdistance))
+            // {
+            //     Debug.DrawRay(transform.position, transform.forward * 2 * rayCastdistance, Color.red);
+            //     PlayerDetection(hitInfoForward);
+            // }
+            // else if (Physics.Raycast(rightRay, out hitInfoLeft, rayCastdistance))
+            // {
+            //     Debug.DrawRay(transform.position, (transform.forward * 2 - transform.right) * rayCastdistance, Color.red);
+            //     PlayerDetection(hitInfoLeft);
+            // }
+            // else if (Physics.Raycast(leftRay, out hitInfoRight, rayCastdistance))
+            // {
+            //     Debug.DrawRay(transform.position, (transform.forward * 2 - (-transform.right)) * rayCastdistance, Color.red);
+            //     PlayerDetection(hitInfoRight);
+            // }
+            // #endregion
+
+            if (visibleTargets.Count != 0)
             {
-                Debug.DrawRay(transform.position, transform.forward * 2 * rayCastdistance, Color.red);
-                PlayerDetection(hitInfoForward);
+                anim.SetTrigger("Chase");
             }
-            else if (Physics.Raycast(rightRay, out hitInfoLeft, rayCastdistance))
-            {
-                Debug.DrawRay(transform.position, (transform.forward * 2 - transform.right) * rayCastdistance, Color.red);
-                PlayerDetection(hitInfoLeft);
-            }
-            else if (Physics.Raycast(leftRay, out hitInfoRight, rayCastdistance))
-            {
-                Debug.DrawRay(transform.position, (transform.forward * 2 - (-transform.right)) * rayCastdistance, Color.red);
-                PlayerDetection(hitInfoRight);
-            }
-            #endregion
 
+        }
+
+        IEnumerator FindTargetsWithDelay(float delay)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(delay);
+                FindVisibleTargets();
+            }
+        }
+
+        void FindVisibleTargets()
+        {
+            visibleTargets.Clear();
+            Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+            for (int i = 0; i < targetsInViewRadius.Length; i++)
+            {
+                Transform target = targetsInViewRadius[i].transform;
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+                {
+                    float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                    if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                    {
+                        visibleTargets.Add(target);
+                    }
+                }
+            }
+        }
+
+        public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+        {
+            if (!angleIsGlobal)
+            {
+                angleInDegrees += transform.eulerAngles.y;
+            }
+            return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
         }
 
         private void PlayerDetection(RaycastHit hitInfo)
@@ -119,8 +177,15 @@ namespace EightDirectionalSpriteSystem
             }
             else
             {
+                // while (NavMesh.SamplePosition(randomPoint, out navHit, 1.0f, 8))
+                // {
+                //     result = randomPoint;
+                //     //Debug.Log("ELSE: " + result);
+                //     return result;
+                // }
+
+                //result = origin;
                 result = randomPoint;
-                //Debug.Log("ELSE: " + result);
                 return result;
             }
         }
