9130f1aa442e4c6eafa287fb77186fa55ab6e202
Christian <chrisarg.otico@gmail.com>
2020-07-01 00:19:12 +0800

    Face UI not done
    Face

---
+++ Project/Assets/Scripts/UI/FaceStatusBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FaceStatusBar : MonoBehaviour
+{
+
+    public GameObject player;
+    protected UnitController unit;
+    public Sprite[] faces;
+
+    //Randomize face 
+    //Face follow Enemy when hit (indicator where the enemy is)
+    //if 20% hp is deducted ouchFace
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        unit = player.GetComponent<UnitController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Faces()
+    {
+        //Random faces when no enemy
+    }
+    private void OnGUI()
+    {
+        
+    }
+}
