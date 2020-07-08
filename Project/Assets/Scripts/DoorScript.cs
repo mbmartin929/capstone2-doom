d2d51b53c80e92bb971a0455265eb39c107bc4ba
Christian <chrisarg.otico@gmail.com>
2020-07-09 00:04:12 +0800

    Door Script Updated

--- Project/Assets/Scripts/DoorScript.cs
+++ Project/Assets/Scripts/DoorScript.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    public GameObject openPanel = null;
-    public float speedOfDoor;
-    public GameObject Door;
+
+    public float speedOfDoor = 0.2f;
     private bool isOpen = false;
-    private bool canOpen = false;
 
 
 
@@ -19,44 +17,17 @@ public class DoorScript : MonoBehaviour
 
     }
 
+    public void ChangeDoorState()
+    {
+        isOpen = true;
+    }
     // Update is called once per frame
     void Update()
     {
-        if(!isOpen)
+
+        if (isOpen)
         {
-            if (Input.GetKeyDown(KeyCode.E) && !isOpen && canOpen)
-            {
-                opening();
-                isOpen = true;
-                Debug.Log("E PRESSED");             
-                //gameObject.transform.Translate(Vector3.up * Time.deltaTime * speedOfDoor);
-               
-
-            }
-        }
-      
-    }
-
-    private void opening()
-    {
-
-        Door.transform.position += Vector3.up * speedOfDoor * Time.deltaTime;
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            canOpen = true;
-            openPanel.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            canOpen = false;
-            openPanel.SetActive(false);
+            transform.position += Vector3.up * speedOfDoor * Time.deltaTime;
         }
     }
 
--- Project/Assets/Scripts/Unit/PlayerController.cs
+++ Project/Assets/Scripts/Unit/PlayerController.cs
@@ -8,26 +8,58 @@ public class PlayerController : UnitController
     protected UnitController unit;
     public bool isDamaged;
 
+    public int rayCastLength;
+
+    [Header("Door function")]
+    bool guiShow = false;
+    bool isOpen = false;
+    //public float speedOfDoor;
+    //public GameObject door;
 
     // Start is called before the first frame update
     void Start()
     {
         unit = player.GetComponent<UnitController>();
         unit.CurHealth = 100;
-        
-     
-     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
 
-       
+        playerRayCast();
+
     }
 
+    void playerRayCast()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit, rayCastLength))
+        {
+            Debug.Log(hit.transform.name);
+            if (hit.collider.gameObject.tag == "Door")
+            {
+                guiShow = true;
+                if (Input.GetKeyDown("e") && isOpen == false)
+                {
+                    hit.collider.transform.GetComponent<DoorScript>().ChangeDoorState();                
+                }
+            }
+        }
+        else
+        {
+            guiShow = false;
+        }
 
+    }
 
-
+    void OnGUI()
+    {
+        //DOOR
+        if (guiShow == true && isOpen == false)
+        {
+            GUI.Box(new Rect(Screen.width / 3, Screen.height / 3, 150, 50), "PRESS " + "E"+ " Open Door");
+        }
+    }
 }
