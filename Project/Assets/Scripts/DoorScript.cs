9f938c67ea92d093f23f638d807d8dbcea3808ea
Christian <chrisarg.otico@gmail.com>
2020-07-10 13:02:48 +0800

    Door script done

--- Project/Assets/Scripts/DoorScript.cs
+++ Project/Assets/Scripts/DoorScript.cs
@@ -8,8 +8,8 @@ public class DoorScript : MonoBehaviour
     public float speedOfDoor = 0.2f;
     private bool isOpen = false;
 
-
-
+    //public Vector3 newYPos;
+    public float ypos;
 
     // Start is called before the first frame update
     void Start()
@@ -24,10 +24,11 @@ public class DoorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        float move = speedOfDoor * Time.deltaTime;
+        Vector3 newYPos = new Vector3(transform.position.x, ypos, transform.position.z);
         if (isOpen)
         {
-            transform.position += Vector3.up * speedOfDoor * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, newYPos, move);
         }
     }
 
