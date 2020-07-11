a17dbbb2323fdef84d37f0012ab737addb9e1949
Christian <chrisarg.otico@gmail.com>
2020-07-11 18:32:56 +0800

    Grenade Script

---
+++ Project/Assets/Scripts/Items/Grenade.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+
+    float radius = 3f;
+    float timer = 2f;
+    float force = 500;
+    float countdown;
+
+    bool hasExplode;
+    GameObject explotionParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0 && !hasExplode)
+        {
+            explode();
+        }
+    }
+
+    void explode()
+    {
+        Debug.Log("KABOOM!");
+        hasExplode = true;
+
+        //instantiate explotion anim
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+            
+            //damage enemy
+        }
+        Destroy(gameObject);
+
+    }
+}
