a0d406a707bc5fd48d1e1119b0560cf88498fbf5
Christian <chrisarg.otico@gmail.com>
2020-10-10 12:48:03 +0800

    AI with diffects

---
+++ Project/Assets/Scripts/_GameJam/Ai/_Scripts/NPCBaseFSMGameJam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NPCBaseFSMGameJam : StateMachineBehaviour 
+{
+
+    public GameObject NPC;
+    public GameObject[] opponent;
+    public UnityEngine.AI.NavMeshAgent agent;
+    public float speed;
+    public float rotSpeed;
+    public float accuracy;
+
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        NPC = animator.gameObject;
+        //GameObject opponent = NPC.GetComponent<VirusAI>().GetPlayer(); /// HERE
+     
+        agent = NPC.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+
+
+        opponent = GameObject.FindGameObjectsWithTag("EnemyGameJam");
+        var target = opponent[Random.Range(0, opponent.Length)].transform;
+    }
+}
---
+++ Project/Assets/Scripts/_GameJam/Ai/_Scripts/VirusAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class VirusAI : MonoBehaviour
+{
+    Animator anim;
+    public GameObject enemy;
+    public GameObject cargo;
+
+
+
+
+    // Start is called before the first frame update
+
+    public GameObject GetOpponent()
+    {
+        return enemy;
+
+    }
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        anim.SetFloat("distance", Vector3.Distance(transform.position, enemy.transform.position));
+    }
+
+  
+
+}
