57ec432c3a570e8624fc9fdba476b24371ab4b88
Christian <chrisarg.otico@gmail.com>
2020-11-04 23:50:07 +0800

    Straight Projectile Script

---
+++ Project/Assets/Scripts/Others/ProjectileStraight.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class ProjectileStraight : MonoBehaviour
+{
+    public GameObject bullet;
+    [SerializeField] private float duration;
+    [HideInInspector] public int damage;
+    public Transform mouthOfAlien;
+
+
+    public float projectileSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        LaunchProjectile();
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void LaunchProjectile()
+    {
+        GameObject proj = Instantiate(bullet, mouthOfAlien.transform.position, transform.rotation);
+        float dist = Vector3.Distance(GameManager.Instance.playerGo.transform.position, mouthOfAlien.position);
+        //GetComponent<Rigidbody>().AddForce(mouthOfAlien.transform.position * projectileSpeed * dist);
+        proj.GetComponent<Rigidbody>().velocity = transform.right * projectileSpeed * dist;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Level"))
+        {
+            if (transform.childCount <= 0)
+            {
+
+            }
+            else if (transform.GetChild(0) != null)
+            {
+                // Debug.Log(transform.GetChild(0).name);
+                transform.GetChild(0).transform.parent = null;
+            }
+
+            //Debug.Log("Collided with Level");
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerController>().TakeDamage(damage);
+
+            Destroy(gameObject);
+        }
+    }
+}
