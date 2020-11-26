1b6f6b3e8589e217711925a9d4b492d241972d9d
mbmartin929 <martinlouie56@gmail.com>
2020-11-27 02:05:00 +0800

    End condition

---
+++ Project/Assets/Scripts/Others/CheckVideo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class CheckVideo : MonoBehaviour
+{
+    private VideoClip videoClip;
+
+    private void Awake()
+    {
+        videoClip = GetComponent<VideoPlayer>().clip;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GoToCredits());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator GoToCredits()
+    {
+        yield return new WaitForSeconds((float)videoClip.length + 0.420f);
+
+        SceneManager.LoadScene("EndMainMenu");
+    }
+}
